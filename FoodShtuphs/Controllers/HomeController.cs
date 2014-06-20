b1ae9f03fd7ac53d3740037d9f6ae0ed98a009bc
ElijahGjerdingen <elijah.gjerdingen@gmail.com>
2014-06-20 14:28:24 -0500

    initial commit

---
+++ FoodShtuphs/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using FoodShtuphs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodShtuphs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.message = "welcome to ASP.NET MVC!";
+
+            var model = new RestaurantReview
+            {
+                Name = "Annie's Parlour",
+                Rating = 9
+            };
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Test";
+            ViewBag.Location = "minnesota, USA";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public int Rating { get; set; }
+    }
+}
---
+++ FoodShtuphs/Models/RestaurantReview.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodShtuphs.Models
+{
+    public class RestaurantReview
+    {
+        public string Name { get; set; }
+        public int Rating { get; set; }
+    }
+}
